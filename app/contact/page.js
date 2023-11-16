4199b3c7a507ed77142ba3cbcc4c56c2bf0bd7ef
CodeTank <114135972+sanujasethi06@users.noreply.github.com>
2023-11-16 16:44:09 +0530

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -10,7 +10,7 @@ const Page = () => {
             <h1>Contact Us</h1>
         <ContactCard />
         <section className={styles.contact_section}>
-          <h2>We'd  love to hear <span>from you</span></h2>
+          <h2>We&apos;d  love to hear <span>from you</span></h2>
           <ContactForm />
 
          
